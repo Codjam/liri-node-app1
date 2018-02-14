b608493c56c7a39ad2200e1a2217a0a4fdbbd30a
Jamal <33230300+Codjam@users.noreply.github.com>
2018-02-13 20:13:27 -0500

    Create keys.js
    adding file

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded' + ' ((((((((((((o)))))))))))) ' + ' ((((((((((((o)))))))))))) ' + ' ((((((((((((o))))))))))))\n\n\n');
+
+exports.twitter = {
+  consumer_key: process.env.cngkdrnKC611hrBqW5pYVnIf4,
+  consumer_secret: process.env.5W4Q3OJmU8wXFAoR3LiIYv9BKCsTXTbGqSkx41iYsIDIqSJY7C,
+  access_token_key: process.env.960310948748845062-v8hdaWCYyJ8uKf47pvPikPVZ57OwJMb,
+  access_token_secret: process.env.5MNdMvZN3xKFxXv3pcCsIj5qhVcP2IRIUfjqvE1faiGFO
+};
+
+exports.spotify = {
+  id: process.env.27a8864ac89a46a0b05ff38922f61f58,
+  secret: process.env.6396ce8169954f6c9edf68c72af394ab
+};
+module.exports = twitterKeys;
